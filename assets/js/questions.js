cd8b5674743ad43db79f61e291a74c37b681518a
frankiesanjana <francesca.sanjana@gmail.com>
2022-05-22 14:30:51 +0000

    Add questions to questions.js page

---
+++ assets/js/questions.js
@@ -0,0 +1,92 @@
+const questions = [
+    {
+        question: 'Who won the Nobel Prize in Physics in 1903 for her contribution to the understanding of radiation?',
+        answers: [
+            { text: 'Marie Curie', correct: true },
+            { text: 'Rosalind Franklin', correct: false },
+            { text: 'Lise Meitner', correct: false },
+            { text: 'Barbara McClintock', correct: false }
+        ]
+    },
+    {
+        question: 'Who is the only woman to have received, by herself (unshared), a Nobel Prize for Medicine?',
+        answers: [
+            { text: 'Rosalyn Sussman Yalow', correct: false },
+            { text: 'Gerty Theresa Cori', correct: false },
+            { text: 'Barbara McClintock', correct: true },
+            { text: 'Rita Levi-Montalcini', correct: false }
+        ]
+    },
+    {
+        question: 'Who was the first woman to go to space?',
+        answers: [
+            { text: 'Kalpana Chawla', correct: false },
+            { text: 'Valentina Tereshkova', correct: true },
+            { text: 'Mae Carol Jemison', correct: false },
+            { text: 'Ellen Ochoa', correct: false }
+        ]
+    },
+    {
+        question: 'Who developed the signal flares that are still used by the U.S. Navy?',
+        answers: [
+            { text: 'Esther Conwell', correct: false },
+            { text: 'Beatrice Hicks', correct: false },
+            { text: 'Beulah Louise Henry', correct: false },
+            { text: 'Martha Coston', correct: true }
+        ]
+    },
+    {
+        question: 'Who was the first girl on the Iranian Mathematical Olympics team?',
+        answers: [
+            { text: 'Hypatia', correct: false },
+            { text: 'Mariam Mirzakhani', correct: true },
+            { text: 'Shakuntala Devi', correct: false },
+            { text: 'Maria Gaetana Agnesi', correct: false }
+        ]
+    },
+    {
+        question: 'Who invented signal hopping, which was a precursor to wi-fi, GPS, and Bluetooth?',
+        answers: [
+            { text: 'Hertha Ayrton', correct: false },
+            { text: 'Emily Warren Roebling', correct: false },
+            { text: 'Edith Clark', correct: false },
+            { text: 'Hedy Lamar', correct: true }
+        ]
+    },
+    {
+        question: 'Who is known as the inventor of the first compiler? (A compiler is a program that translates programming code to machine language).',
+        answers: [
+            { text: 'Grace Hopper', correct: true },
+            { text: 'Anita Borg', correct: false },
+            { text: 'Adele Goldstine', correct: false },
+            { text: 'Marsha Rhea Williams', correct: false }
+        ]
+    },
+    {
+        question: 'Who has been nicknamed “the mother of the internet”?',
+        answers: [
+            { text: 'Evelyn Boyd Granville', correct: false },
+            { text: 'Beatrice Helen Worsley', correct: false },
+            { text: 'Radia Perlman', correct: true },
+            { text: 'Carol Shaw', correct: false }
+        ]
+    },
+    {
+        question: 'Who studied biology, started her career in healthcare finance and later went on to become CEO of a leading personal genomics company?',
+        answers: [
+            { text: 'Anne Wojcicki', correct: true },
+            { text: 'Sheryl Sandberg', correct: false },
+            { text: 'Susan Wojcicki', correct: false },
+            { text: 'Jennifer Taubert', correct: false }
+        ]
+    },
+    {
+        question: 'Who is the founder of Girls Who Code?',
+        answers: [
+            { text: 'Rachel Carson', correct: false },
+            { text: 'Elizabeth Feinler', correct: false },
+            { text: 'Bozoma Saint John', correct: false },
+            { text: 'Reshma Saujani', correct: true }
+        ]
+    }
+];
