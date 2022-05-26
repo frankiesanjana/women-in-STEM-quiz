83142b9f330c3c10ce95a869fdc56359d959848f
frankiesanjana <francesca.sanjana@gmail.com>
2022-05-26 11:09:28 +0000

    Add basic variables to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
 // define variables to select elements
 
 const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questions, currentQuestionIndex;
 
 // define variables to drive quiz
+
+let currentQuestionIndex;
+let currentCorrect = 0;
+let currentIncorrect = 0;
 
 // functions
 
 function startQuiz() {
-
+    currentQuestionIndex = 0
 }
 
 function displayNextQuestion() {
@@ -27,5 +33,5 @@ function incrementIncorrect() {
 }
 
 function endQuiz() {
-    
+
 }
