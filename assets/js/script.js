68f34e729a0d65328762d2f92f6d36beff99f4cf
frankiesanjana <francesca.sanjana@gmail.com>
2022-05-26 10:47:28 +0000

    Add basic structure of quiz functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// define variables to select elements
+
+const startButton = document.getElementById("start-btn");
+
+// define variables to drive quiz
+
+// functions
+
+function startQuiz() {
+
+}
+
+function displayNextQuestion() {
+
+}
+
+function checkAnswer() {
+    
+}
+
+function incrementCorrect() {
+    
+}
+
+function incrementIncorrect() {
+    
+}
+
+function endQuiz() {
+    
+}
